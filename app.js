2fc0552795fb841420df29af6f17dd27471c6802
Aslam Rasheed <aslampr07@outlook.com>
2017-12-28 10:25:50 +0530

    Added Visual Studio code directory ot gitignore file. The middlewere urlencoded is used to parse the url queries. The implementation of the GET request for the getting the nearby restaurents.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var con = require('./config/db');
 
 var app = express();
 
+app.use(express.urlencoded({extended:true}));
+
 app.use('/api/1.0/restaurant', require('./routes/restaurant')(con));
 
 app.listen(8000);
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var mysql = require('mysql');
 
 module.exports = function(con){
     var router = express.Router();
     router.get('/', function(req, res){
-        res.send("Hello World");
+        if(req.query.lat && req.query.lon && req.query.count){
+            var latitude = parseFloat(req.query.lat);
+            var longitude = parseFloat(req.query.lon);
+            var count = parseInt(req.query.count);
+            
+            var sql = mysql.format("SELECT name, code, calculate_distance(latitude, longitude, ?, ?)" +
+             " AS distance FROM Hotel ORDER BY distance LIMIT ?", [latitude, longitude, count]);
+            con.query(sql, function(err, rows){
+                if(err)
+                    throw err;
+                var response = {
+                    status: 'success',
+                    result: rows
+                }
+                res.send(response);
+            })
+        }
     });
     return router;
 }
