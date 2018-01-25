37f12d44ed85deefcbded71ff50b4d5cdc6722f7
Aslam Rasheed <aslampr07@outlook.com>
2018-01-25 18:50:12 +0530

    removed the signup and survey page

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ app.use(express.urlencoded({extended:true}));
 //WebPage
 
 app.use('/', express.static('public'));
-app.use('/register', express.static('public/register'));
-app.use('/survey', express.static('public/survey'));
+//removed the register and signup page.
+//app.use('/register', express.static('public/register'));
+//app.use('/survey', express.static('public/survey'));
 
 //Routes
 app.use('/api/1.0/restaurant', require('./routes/restaurant')(con));
 app.use('/api/1.0/items', require('./routes/items')(con));
+//Depracted items account.js and survey.js
 app.use('/api/1.0/account', require('./routes/account')(con));
 app.use('/api/1.0/survey', require('./routes/survey')(con));
 
