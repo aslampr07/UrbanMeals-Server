2d48a0e541b7238cfad38493af878301cf73dd8f
Aslam Rasheed <aslampr07@outlook.com>
2018-03-08 21:33:07 +0530

    The nearby service now require the authentication token, added verification.js to verify

---
+++ tools/verification.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+
+module.exports = {
+    verify : function(con, token, callback){
+        var response = {};
+        console.log(token)
+        var sql = mysql.format('SELECT phoneVerified as verified FROM User u, Login_Session WHERE userID = u.ID AND sessionToken = ?', [token]);
+        con.query(sql, function(err, rows){
+            if(rows.length <= 0){
+                response.status = 'error';
+                response.type = [113];
+            }
+            else{
+                if(rows[0].verified == 'N'){
+                    response.status = 'error';
+                    response.type = [114];
+                }
+                else{
+                    response.status = 'success';
+                }
+            }
+            callback(response);
+        });
+    }
+}
