b2a4c59d8bfaa742a4930163e55845f470c8f153
Aslam Rasheed <aslampr07@outlook.com>
2018-03-18 17:19:11 +0530

    Added the getter for categories and Items.

--- routes/items.js
+++ routes/items.js
@@ -4,105 +4,107 @@
 var express = require('express');
 var mysql = require('mysql');
 
-module.exports = function(con){
+module.exports = function (con) {
     var router = express.Router();
-    
-    //For getting the all list of categories along with there IDs
-    router.get('/categories', function(req, res){
-        var sql = 'SELECT * FROM Category';
-        con.query(sql, function(err, result){
-            if(err)
-            var response = {
-                'status':'success',
-                'result':result
-            };
-            res.send(result);
-        });
-    });
 
-    router.get('/find', function(req, res){
-        var hotelCode = req.query.hotelCode;
+    //To get the categories that displays below the Digital Menu.
+    router.get("/digitalmenu/categories", function (req, res) {
+        var hotelCode = req.query.hotelcode;
 
-        var sql = mysql.format('SELECT ID FROM Hotel WHERE code = ?', [hotelCode]);
-        con.query(sql, function(err, result){
-            if(err)
-            {
+        var sql = mysql.format("SELECT * FROM Hotel WHERE code = ?", [hotelCode]);
+        con.query(sql, function (err, rows) {
+            if (err) {
                 throw err;
             }
-            if(result.length){
-                var hotelId = result[0].ID;
-                
-                var sql = mysql.format('SELECT i.ID, i.name, cm.categoryID AS category, p.ID AS priceID, p.amount, p.description '+
-                'from Item i, Category_Map cm, Price p'+
-                ' WHERE p.itemID = i.ID AND cm.itemID = i.ID AND i.hotelID = ? ORDER BY i.ID', [hotelId]);
-                console.log(sql);
-                con.query(sql, function(err, result){
-                    if(err){
+            if (rows.length > 0) {
+                var sql = mysql.format("SELECT ID, name, image as imageURL from Menu_Categories, Category " +
+                    "WHERE hotelID =" +
+                    "(SELECT ID FROM Hotel WHERE code = ? ) AND categoryID = ID", [hotelCode]);
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
-                    var response = [];
-                    for(var i in result){
-                        var index = CheckIdExist(response, result[i].ID);
-                        if(index == -1) {
-                            var insertData = {
-                                'ID':result[i].ID,
-                                'name':result[i].name,
-                                'category':[result[i].category],
-                                'price':[{
-                                    'ID':result[i].priceID,
-                                    'amount':result[i].amount,
-                                    'description':result[i].description
-                                }]
-                            };
-                        response.push(insertData);
-                       }
-                       else{
-                            if(CheckCategoryExist(response[index].category, result[i].category) == -1){
-                                response[index].category.push(result[i].category);
-                            }
-                            
-                            if(CheckPriceExist(response[index].price, result[i].priceID) == -1){
-                                var price = {
-                                    'ID':result[i].priceID,
-                                    'amount':result[i].amount,
-                                    'description':result[i].description
-                                };
-                                response[index].price.push(price);
-                            }
-                       }
+                    //Push the 'All' category to starting of the array.
+                    rows.unshift({ 'ID': 1, 'name': 'All', 'imageURL': '/assets/categoryimages/default.png' });
+                    var response = {
+                        'status': 'success',
+                        'result': rows
                     }
                     res.send(response);
                 });
-                
             }
-            else
-                res.send("The hotelCode is invalid");
+            else {
+                var respone = {
+                    'status': 'error',
+                    'type': [115]
+                }
+                res.send(respone);
+            }
         });
     });
 
-    function CheckIdExist(itemList, itemId){
-        for(var i in itemList){
-            if(itemList[i].ID == itemId)
-                return i;
-        }
-        return -1;
-    }
+    router.get("/get", function (req, res) {
+        var categoryID = req.query.categoryid;
+        var hotelCode = req.query.hotelcode;
+        var token = req.query.token;
 
-    function CheckCategoryExist(categories, catId){
-        for(var i in categories){
-            if(categories[i] == catId)
-                return i;
+        if (categoryID == 1) {
+            var sql = mysql.format("SELECT * FROM Hotel WHERE code = ?", [hotelCode]);
+            con.query(sql, function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                if (rows.length > 0) {
+                    var sql = mysql.format("SELECT name, min(p.amount) as price FROM Item i, Price p WHERE itemID = i.ID and i.hotelId = ? GROUP BY itemID", [rows[0].ID]);
+                    con.query(sql, function(err, result){
+                        if(err){
+                            throw err;
+                        }
+                        var respone = {
+                            'status' : 'success',
+                            'result' : result
+                        };
+                        res.json(respone);
+                    });
+                }
+                else {
+                    var respone = {
+                        'status': 'error',
+                        'type': [115]
+                    }
+                    res.json(respone);
+                }
+            });
         }
-        return -1;
-    }
+        else if(categoryID > 1){
+            var sql = mysql.format("SELECT * FROM Hotel WHERE code = ?", [hotelCode]);
+            con.query(sql, function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                if (rows.length > 0) {
+                    var sql = mysql.format("SELECT name, MIN(amount) as price FROM Item i, Price p WHERE i.ID in (SELECT cm.ItemID FROM Category_Map cm WHERE cm.CategoryID = ?) AND i.ID = p.itemID and i.hotelID = ? GROUP BY i.ID", [categoryID, rows[0].ID]);
+                    con.query(sql, function(err, result){
+                        if(err){
+                            throw err;
+                        }
+                        var respone = {
+                            'status' : 'success',
+                            'result' : result
+                        };
+                        res.json(respone);
+                    });
+                }
+                else {
+                    var respone = {
+                        'status': 'error',
+                        'type': [115]
+                    }
+                    res.json(respone);
+                }
+            });
+        }
+    });
 
-    function CheckPriceExist(prices, priceId){
-        for(var i in prices){
-            if(prices[i].ID == priceId)
-                return i;
-        }
-        return -1;
-    }
-    
     return router;
 }
