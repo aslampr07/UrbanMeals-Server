8b2d7f9a9cb8791063076d41fedc60e139adc246
Aslam Rasheed <aslampr07@outlook.com>
2018-06-02 17:08:41 +0530

    Phone number verification is not required.

--- tools/verification.js
+++ tools/verification.js
@@ -1,23 +1,25 @@
 "use strict";
 var mysql = require('mysql');
 module.exports = {
-    verify : function(con, token, callback){
+    verify: function (con, token, callback) {
         var response = {};
         var sql = mysql.format('SELECT u.ID, phoneVerified as verified FROM User u, Login_Session WHERE userID = u.ID AND sessionToken = ?', [token]);
-        con.query(sql, function(err, rows){
-            if(err){
+        con.query(sql, function (err, rows) {
+            if (err) {
                 throw err;
             }
-            if(rows.length <= 0){
+            if (rows.length <= 0) {
                 response.status = 'error';
                 response.type = [113];
             }
-            else{
-                if(rows[0].verified == 'N'){
+            else {
+                //if(rows[0].verified == 'N'){
+                //The user does not need phone verification.
+                if (false) {
                     response.status = 'error';
                     response.type = [114];
                 }
-                else{
+                else {
                     response.status = 'success';
                     response.id = rows[0].ID;
                 }
