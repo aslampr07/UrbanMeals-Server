56375a775fa43242d2fee5479c08ce9448cfbbcb
Aslam Rasheed <aslampr07@outlook.com>
2018-06-16 14:51:40 +0530

    Now return the hotel Images.

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -22,7 +22,7 @@ module.exports = function (con) {
                         
                     var sql = mysql.format("SELECT name, code, calculate_distance(latitude, longitude, ?, ?)" +
                         " AS distance, type, openingTime, closingTime, (SELECT AVG(rating) FROM Hotel_Rating WHERE hotelID = h.ID) as rating FROM Hotel h ORDER BY distance LIMIT ?", [latitude, longitude, count]);*/
-                        var sql = mysql.format("SELECT name, code, calculate_distance(latitude, longitude, ?, ?)" +
+                    var sql = mysql.format("SELECT name, code, calculate_distance(latitude, longitude, ?, ?)" +
                         " AS distance, type, openingTime, closingTime, (SELECT AVG(rating) FROM Hotel_Rating WHERE hotelID = h.ID) as rating FROM Hotel h WHERE enabled = 'Y' ORDER BY distance", [latitude, longitude]);
                     con.query(sql, function (err, rows) {
                         if (err)
@@ -39,10 +39,10 @@ module.exports = function (con) {
                             item.type = rows[i].type;
 
                             //If the rating is null, then set it to zero.
-                            if(!rows[i].rating){
+                            if (!rows[i].rating) {
                                 item.rating = 0;
                             }
-                            else{
+                            else {
                                 item.rating = Number(rows[i].rating.toFixed(2));
                             }
 
@@ -63,57 +63,57 @@ module.exports = function (con) {
                     });
 
                     var sql = mysql.format("INSERT INTO User_Locations VALUES(?, ?, ?, now())", [status.id, latitude, longitude]);
-                    con.query(sql, function(err){
+                    con.query(sql, function (err) {
                     })
                 }
-                else if(status.status == 'error'){
+                else if (status.status == 'error') {
                     res.send(status);
                 }
             });
             //Retrieves the items
         }
-    }); 
+    });
 
     //Adds rating to the hotels
-    router.post('/rating', function(req, res){
+    router.post('/rating', function (req, res) {
         var token = String(req.query.token);
-        
+
         var hotelCode = String(req.body.hotelcode);
         var rating = Number(req.body.rating);
 
-        tokenVerify.verify(con, token, function(check){
-            if(check.status == 'success'){
+        tokenVerify.verify(con, token, function (check) {
+            if (check.status == 'success') {
                 var userID = check.id;
                 var sql = mysql.format("SELECT ID FROM Hotel WHERE code = ?", [hotelCode]);
-                con.query(sql, function(err, rows){
-                    if(err){
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
-                    if(rows.length > 0){
+                    if (rows.length > 0) {
                         var hotelID = rows[0].ID;
 
                         var sql = mysql.format("INSERT INTO Hotel_Rating(userID, hotelID, rating, creationTime) VALUES (?, ?, ?, NOW()) ON DUPLICATE KEY UPDATE rating = ?, creationTime = NOW()", [userID, hotelID, rating, rating]);
-                        con.query(sql, function(err, rows){
-                            if(err){
+                        con.query(sql, function (err, rows) {
+                            if (err) {
                                 throw err;
                             }
                             var response = {
-                                'status' : 'success'
+                                'status': 'success'
                             }
                             res.json(response);
                         })
                     }
-                    else{
+                    else {
                         var response = {
-                            'status' : 'error',
-                            'type' : [115]
+                            'status': 'error',
+                            'type': [115]
                         }
                         res.json(response);
                     }
                 });
 
             }
-            else{
+            else {
                 res.json(check);
             }
         });
@@ -121,41 +121,41 @@ module.exports = function (con) {
 
 
     //Get the rating from the hotel
-    router.get('/rating', function(req, res){
+    router.get('/rating', function (req, res) {
         var hotelCode = String(req.query.hotelcode);
-        
+
         var sql = mysql.format("SELECT ID FROM Hotel WHERE code = ?", [hotelCode]);
-        con.query(sql, function(err, rows){
-            if(err){
+        con.query(sql, function (err, rows) {
+            if (err) {
                 throw err;
             }
-            if(rows.length > 0){
+            if (rows.length > 0) {
                 var hotelID = rows[0].ID;
                 var sql = mysql.format("SELECT AVG(rating) as rating FROM Hotel_Rating WHERE hotelID = ?", [hotelID]);
-                con.query(sql, function(err, rows){
-                    if(err){
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
-                    if(rows[0].rating == null){
+                    if (rows[0].rating == null) {
                         var response = {
-                            'status' : 'success',
-                            'rating' : 0.0
+                            'status': 'success',
+                            'rating': 0.0
                         }
                         res.json(response);
                     }
-                    else{
+                    else {
                         var response = {
-                            'status' : 'success',
-                            'rating' : Number(rows[0].rating.toFixed(2))
+                            'status': 'success',
+                            'rating': Number(rows[0].rating.toFixed(2))
                         }
                         res.json(response);
                     }
                 });
             }
-            else{
+            else {
                 var response = {
-                    'status' : 'error',
-                    'type' : [115]
+                    'status': 'error',
+                    'type': [115]
                 }
                 res.json(response);
             }
@@ -166,96 +166,108 @@ module.exports = function (con) {
     /**
      * @deprecated
      */
-    router.get('/profile', function(req, res){
+    router.get('/profile', function (req, res) {
         var hotelCode = String(req.query.hotelcode);
         var token = String(req.query.token);
 
-        tokenVerify.verify(con, token, function(check){
-            if(check.status == 'success'){
+        tokenVerify.verify(con, token, function (check) {
+            if (check.status == 'success') {
                 var sql = mysql.format("SELECT ID FROM Hotel WHERE code = ?", [hotelCode]);
-                con.query(sql, function(err, rows){
-                    if(err){
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
-                    if(rows.length > 0){
+                    if (rows.length > 0) {
                         var hotelID = rows[0].ID;
-                        var sql = mysql.format("SELECT name, place, openingTime, closingTime, phone, latitude, longitude, street, city, pincode, body from Hotel, Hotel_Profile p, Hotel_Description d WHERE ID = ? AND ID = p.hotelID AND ID = d.hotelID", [hotelID]);
-                        con.query(sql, function(err, rows){
-                            if(err){
+                        let sql = mysql.format("SELECT name, place, openingTime, closingTime, phone, latitude, longitude, street, city, pincode, body from Hotel, Hotel_Profile p, Hotel_Description d WHERE ID = ? AND ID = p.hotelID AND ID = d.hotelID", [hotelID]);
+                        con.query(sql, function (err, rows) {
+                            if (err) {
                                 throw err;
                             }
                             var result = {
-                                'name' : rows[0].name,
-                                'place' : rows[0].place,
-                                'phone' : rows[0].phone,
-                                'latitude' : rows[0].latitude,
-                                'longitude' : rows[0].longitude,
-                                'description' : rows[0].body
+                                'name': rows[0].name,
+                                'place': rows[0].place,
+                                'phone': rows[0].phone,
+                                'latitude': rows[0].latitude,
+                                'longitude': rows[0].longitude,
+                                'description': rows[0].body
                             };
                             result.isOpened = isTimeBetween(rows[0].openingTime, rows[0].closingTime);
-                            result.address = rows[0].street +", "+rows[0].city+"\nPIN-"+rows[0].pincode;
+                            result.address = rows[0].street + ", " + rows[0].city + "\nPIN-" + rows[0].pincode;
                             result.openingTime = moment(rows[0].openingTime, "hh:mm:ss").format("h:mm A");
-                            result.closingTime = moment(rows[0].closingTime, "hh:mm:ss").format("h:mm A");                            
-                            var response = {
-                                'status' : 'success',
-                                'result' : result
-                            };
-                            res.json(response);
-                            console.log("Accessed Hotel Profile");
+                            result.closingTime = moment(rows[0].closingTime, "hh:mm:ss").format("h:mm A");
+
+                            //For retrieving the images of the Hotels
+                            let sql = mysql.format("SELECT imageURL from Hotel_Pictures WHERE hotelID = ?", [hotelID]);
+                            con.query(sql, function (err, rows) {
+                                if (err) {
+                                    throw err;
+                                }
+                                result.images = rows.map(function(obj){
+                                    return obj.imageURL
+                                });
+                                let response = {
+                                    'status': 'success',
+                                    'result': result
+                                };
+                                res.json(response);
+                            })
+
+
                         });
                     }
-                    else{
+                    else {
                         var response = {
-                            'status' : 'error',
-                            'type' : [115]
+                            'status': 'error',
+                            'type': [115]
                         };
                         res.json(response);
                     }
                 });
             }
-            else{
+            else {
                 res.json(check);
             }
         })
     });
 
-    router.get('/search/suggestion', function(req, res){
+    router.get('/search/suggestion', function (req, res) {
         let token = req.query.token;
         let query = String(req.query.query);
 
-        tokenVerify.verify(con, token, function(report){
-            if(report.status == "success"){
+        tokenVerify.verify(con, token, function (report) {
+            if (report.status == "success") {
                 let sql = mysql.format("SELECT name, code, place FROM Hotel, Hotel_Profile WHERE hotelID = ID AND (MATCH(name) AGAINST (? IN NATURAL LANGUAGE MODE) OR SOUNDEX(name) = SOUNDEX(?) OR name LIKE ?)", [query, query, '%' + query + '%']);
-                con.query(sql, function(err, rows){
-                    if(err){
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
                 });
-                con.query(sql, function(err, rows){
-                    if(err){
+                con.query(sql, function (err, rows) {
+                    if (err) {
                         throw err;
                     }
                     let response = {
-                        "status" : "success",
-                        "result" : rows
+                        "status": "success",
+                        "result": rows
                     }
                     res.json(response);
                 });
             }
-            else{
+            else {
                 res.json(report);
             }
         });
     });
 
-    function isTimeBetween(start, end){
+    function isTimeBetween(start, end) {
         var now = moment();
         var a = moment(start, 'hh:mm:ss');
         var b = moment(end, 'hh:mm:ss');
-        if(now.isBetween(a, b)){
+        if (now.isBetween(a, b)) {
             return true
         }
-        else{
+        else {
             return false;
         }
     }
