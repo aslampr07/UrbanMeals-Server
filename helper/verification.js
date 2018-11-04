6e4cd75dd310546f93199968203aca6b43bb73a2
Aslam Rasheed <aslampr07@outlook.com>
2018-11-04 22:02:23 +0530

    Added the Manager initializing end-point.

--- helper/verification.js
+++ helper/verification.js
@@ -1,19 +1,110 @@
 let mysql = require("mysql")
 
 class Verfication {
+
     constructor(con) {
         this._con = con
     }
-    verifyToken(token){
-        let sql = mysql.format("SELECT * FROM Login_Session WHERE sessionToken = ?", [token])
+
+    //verify wheather the session token is valid.
+    verifyToken(token) {
+        let sql = mysql.format("SELECT userID FROM Login_Session WHERE sessionToken = ?", [token])
+        return new Promise((resolve, reject) => {
+            this._con.query(sql, (err, rows) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    if (rows.length > 0) {
+                        resolve(rows[0].userID)
+                    }
+                    else {
+                        resolve(null)
+                    }   
+                }
+            });
+        });
+    }
+
+    //For retrieving the hotelID from the hotelCode
+    getHotelID(hotelCode){
+        let sql = mysql.format("SELECT ID FROM Hotel WHERE code = ?", [hotelCode]);
         return new Promise((resolve, reject) => {
             this._con.query(sql, (err, rows) => {
                 if(err){
                     reject(err)
                 }
-                resolve(rows[0]);
+                else{
+                    if(rows.length > 0){
+                        resolve(rows[0].ID)
+                    }   
+                    else{
+                        resolve(null);
+                    }      
+                }
             });
         });
-   }
+    }
+
+    //for finding wheather the user is admin or not of an hotel.
+    isHotelAdmin(userID, hotelID){
+        let sql = mysql.format("SELECT * from Hotel_Admins WHERE userID = ? AND hotelID = ?", [userID, hotelID]);
+        return new Promise((resolve, reject) => {
+            this._con.query(sql, (err, rows) => {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    if(rows.length > 0){
+                        resolve(true);
+                    }
+                    else{
+                        resolve(false);
+                    }
+                }
+            })
+        });
+    }
+
+
 }
-module.exports = {Verfication}
+
+class HotelAdmin{
+    constructor(con){
+        this._con = con;
+    }
+
+    getHotelProfileAdmin(hotelID, userID) {
+        let sql = mysql.format(`SELECT name, code, CONCAT(firstname, " ", lastname) AS admin, street, place, city, pincode, hp.phone as phone, body as description
+                                FROM Hotel h, User_Profile u, Hotel_Admins ha, Hotel_Profile hp, Hotel_Description hd 
+                                WHERE ha.userID = ? 
+                                AND ha.hotelID = ? 
+                                AND ha.userID = u.userID 
+                                AND ha.hotelID = h.ID 
+                                AND hp.hotelID = h.ID 
+                                AND hd.hotelID = h.ID;`, [userID, hotelID]);
+        return new Promise((resolve, reject) => {
+            this._con.query(sql, (err, rows) => {
+                if(err){
+                    reject(err)
+                }
+                else{
+                    let response = rows[0];
+                    let sql = mysql.format(`SELECT imageURL FROM Hotel_Pictures WHERE hotelID =?`,[hotelID]);
+                    this._con.query(sql, (err, rows) => {
+                        if(err){
+                            reject(err)
+                        }
+                        else{
+                            response.images = rows.map(obj => {
+                                return obj.imageURL;
+                            });
+                            resolve(response);
+                        }
+                    });
+                }
+            });
+        });
+    }
+}
+module.exports = {Verfication, HotelAdmin}
--- routes/v1.1/admin.js
+++ routes/v1.1/admin.js
@@ -3,11 +3,12 @@
 let express = require("express")
 let verification = require("../../tools/verification")
 let mysql = require('mysql');
-let v = require("../../helper/verification")
+let urbanmeals = require("../../helper/verification")
 
 module.exports = function (con) {
     var router = express.Router();
-    let verify = new v.Verfication(con)
+    let verify = new urbanmeals.Verfication(con);
+    let hotel = new urbanmeals.HotelAdmin(con);
 
 
     router.get("/1.1/verify", function (req, res) {
@@ -41,13 +42,57 @@ module.exports = function (con) {
         });
     })
 
-    router.get("/1.1/init", async function(req, res){
+    router.get("/1.1/init", async function (req, res) {
         let hotelCode = req.query.hotelcode;
         let token = req.query.token;
 
-        let id = await verify.verifyToken(token)
-        console.log(id)
-    })
+        try {
+            let id = await verify.verifyToken(token)
+            if(id == null){
+                //when session token is not found.
+                let response = {
+                    success : false,
+                    type : [113]
+                }
+                res.json(response)
+            }
+            else{
+                let hotelID = await verify.getHotelID(hotelCode)
+                if(hotelID == null){
+                    //hotelcode does not match any hotel
+                    let response = {
+                        success : false,
+                        type : [115]
+                    }
+                    res.send(response)
+                }
+                else{
+                    let isAdmin = await verify.isHotelAdmin(id, hotelID);
+                    //Checking wheather the user is admin or not.
+                    if(isAdmin){
+                        //Do everything.
+                        let result = await hotel.getHotelProfileAdmin(hotelID, id);
+                        let response = {
+                            success : true,
+                            result : result
+                        }
+                        res.json(response);
+                    }
+                    else{
+                        let response = {
+                            success : false,
+                            type : [120]
+                        }
+                        res.send(response)
+                    }
+                }
+            }
+        }
+        catch(err){
+            console.log("Error occurred");
+            throw err;
+        }
+    });
 
     return router;
 }
